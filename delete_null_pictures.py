1510019a574231dec8672b0e8da1864e17dfd260
mengqi <mengqi.li0907@gmail.com>
2024-06-19 18:27:02 -0400

    delete null pictures

---
+++ delete_null_pictures.py
@@ -0,0 +1,26 @@
+import os
+import rasterio
+
+def filter_nodata(hr_input_path):
+
+    count = 0
+ 
+    # 遍历输入目录中的所有文件
+    for filename in os.listdir(hr_input_path):
+        if filename.endswith(".tif"):  # 确保处理的是 TIFF 文件
+            file_path = os.path.join(hr_input_path, filename)
+            with rasterio.open(file_path) as src:
+                nodata_value = src.nodata
+                if nodata_value is not None:
+                    data = src.read(1)
+                    if not (data == nodata_value).any():  # 检查第一个波段是否包含 nodata
+                        print(f"File {filename} contains nodata value {nodata_value}")
+                        count += 1
+                        # os.remove(file_path)
+
+    print(f"Found {count} files with nodata values")
+# 定义路径
+hr_input_path = 'dataset/LR'
+# Found 7357 files with nodata values
+# 调用函数
+filter_nodata(hr_input_path)
